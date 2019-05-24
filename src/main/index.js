ae85363cc1dc79aee0750c2a5ab45a456e76277c
namo <nhkimnamo@gmail.com>
2019-05-24 16:07:16 +0900

    Create Menu skeleton

--- src/main/index.js
+++ src/main/index.js
@@ -1,10 +1,43 @@
 const { app } = require("electron");
 import createMainWindow from "./createMainWindow";
+import setAppMenu from "./setAppMenu";
 
 let mainWindow = null;
 
+function openTemplateFile() {
+    console.log("openTemplateFile");
+}
+
+function openContextFile() {
+    console.log("openContextFile");
+}
+
+function saveTemplateFile() {
+    console.log("saveTemplateFile");
+}
+
+function saveContextFile() {
+    console.log("saveContextFile");
+}
+
+function saveAsNewContextFile() {
+    console.log("saveAsNewContextFile");
+}
+
+function saveAsNewTemplateFile() {
+    console.log("saveAsNewTemplateFile");
+}
+
 app.on("ready", () => {
     mainWindow = createMainWindow();
+    setAppMenu({
+        openTemplateFile,
+        openContextFile,
+        saveTemplateFile,
+        saveContextFile,
+        saveAsNewContextFile,
+        saveAsNewTemplateFile
+    });
 });
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
---
+++ src/main/setAppMenu.js
@@ -0,0 +1,102 @@
+import { Menu } from "electron";
+
+let isMac = process.platform === 'darwin';
+
+function setAppMenu(options) {
+    const template = [
+        // { role: 'appMenu' }
+        ...(isMac ? [{
+            label: "Serami",
+            submenu: [
+                { role: 'about' },
+                { type: 'separator' }, //////////
+                { role: 'services' },
+                { type: 'separator' }, //////////
+                { role: 'hide' },
+                { role: 'hideothers' },
+                { role: 'unhide' },
+                { type: 'separator' }, //////////
+                { role: 'quit' }
+            ]
+        }] : []),
+        // { role: 'fileMenu' }
+        {
+            label: 'File',
+            submenu: [
+                { label: "Open Template", accelerator: "CmdOrCtrl+O", click: () => options.openTemplateFile() },
+                { label: "Open Context", accelerator: "CmdOrCtrl+Shift+O", click: () => options.openContextFile() },
+                { type: 'separator' }, //////////
+                { label: "Save Template", accelerator: "CmdOrCtrl+S", click: () => options.saveTemplateFile() },
+                { label: "Save Context", accelerator: "CmdOrCtrl+Shift+S", click: () => options.saveContextFile() },
+                { label: "Save As Context", click: () => options.saveAsNewContextFile() },
+                { label: "Save As Template", click: () => options.saveAsNewTemplateFile() },
+                { type: 'separator' },
+                isMac ? { role: 'close' } : { role: 'quit' },
+            ]
+        },
+        // { role: 'editMenu' }
+        {
+            label: 'Edit',
+            submenu: [
+                { role: 'undo' },
+                { role: 'redo' },
+                { type: 'separator' }, //////////
+                { role: 'cut' },
+                { role: 'copy' },
+                { role: 'paste' },
+                ...(isMac ? [
+                    { role: 'pasteAndMatchStyle' },
+                    { role: 'delete' },
+                    { role: 'selectAll' },
+                    { type: 'separator' }, //////////
+                    {
+                        label: 'Speech',
+                        submenu: [
+                            { role: 'startspeaking' },
+                            { role: 'stopspeaking' }
+                        ]
+                    }
+                ] : [
+                    { role: 'delete' },
+                    { type: 'separator' }, //////////
+                    { role: 'selectAll' }
+                ])
+            ]
+        },
+        // { role: 'viewMenu' }
+        {
+            label: 'View',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { role: 'toggledevtools' },
+                { type: 'separator' }, //////////
+                { role: 'resetzoom' },
+                { role: 'zoomin' },
+                { role: 'zoomout' },
+                { type: 'separator' }, //////////
+                { role: 'togglefullscreen' }
+            ]
+        },
+        // { role: 'windowMenu' }
+        {
+            label: 'Window',
+            submenu: [
+                { role: 'minimize' },
+                { role: 'zoom' },
+                ...(isMac ? [
+                    { type: 'separator' }, //////////
+                    { role: 'front' },
+                    { type: 'separator' }, //////////
+                    { role: 'window' }
+                ] : [
+                    { role: 'close' }
+                ])
+            ]
+        },
+    ];
+    const menu = Menu.buildFromTemplate(template);
+    Menu.setApplicationMenu(menu);
+}
+
+export default setAppMenu;
